fb85aa3129b25219c10cbf1c533292045211be5b
blossom-babs <saphashb@gmail.com>
2025-05-02 15:30:30 -0400

    make root a server side component

--- app/(auth)/Layout.tsx
+++ app/(auth)/Layout.tsx
@@ -1,8 +1,14 @@
 import React, { ReactNode } from "react";
-import AuthLayoutServer from "./AuthLayout.server";
+import { redirect } from "next/navigation";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <AuthLayoutServer>{children}</AuthLayoutServer>;
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+
+  const isUserAuthenticated = await isAuthenticated()
+
+  if(isUserAuthenticated) redirect('/')
+
+  return <div>{children}</div>;
 };
 
 export default AuthLayout;
--- app/(root)/Layout.tsx
+++ app/(root)/Layout.tsx
@@ -1,15 +1,19 @@
-'use client'
 import Link from "next/link";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode } from "react";
 import Image from "next/image";
 import { isAuthenticated } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
 
-const RootLayout =  ({ children }: { children: ReactNode}) => {
+const RootLayout =   async ({ children }: { children: ReactNode}) => {
 
+   const isUserAuthenticated = await isAuthenticated()
   
+    if (!isUserAuthenticated) {
+      redirect("/sign-in");
+  }
 
   return (
+
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
@@ -25,6 +29,7 @@ const RootLayout =  ({ children }: { children: ReactNode}) => {
       </nav>
       {children}
     </div>
+  
   );
 };
 
